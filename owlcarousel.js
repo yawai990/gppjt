de2049eda5f96b68ed424a09197e47a51c7faf75
yawai990 <ywaung990@gmail.com>
2022-02-11 19:11:16 +0630

    first commit

--- owlcarousel.js
+++ owlcarousel.js
@@ -3,27 +3,28 @@ const owlSingleCards = document.querySelectorAll('.owl-card');
 
 let index = 0;
 
-console.log(owlSingleCards.length);
+window.addEventListener('load', () => {
 
-owlCarouselWrapper.style.transition = `all 0.3s ease`;
-owlCarouselWrapper.style.transform = `translateX(-${(owlSingleCards[index].clientWidth * index) + (owlSingleCards[index].clientWidth * 2)}px)`;
-
-setInterval(() => {
-    if (index > owlSingleCards.length - 3) return
-    index++
-    checkIndex()
     owlCarouselWrapper.style.transition = `all 0.3s ease`;
     owlCarouselWrapper.style.transform = `translateX(-${(owlSingleCards[index].clientWidth * index) + (owlSingleCards[index].clientWidth * 2)}px)`;
-}, 3000);
 
-function checkIndex() {
-    owlCarouselWrapper.addEventListener('transitionend', () => {
+    setInterval(() => {
+        if (index > owlSingleCards.length - 3) return
+        index++
+        checkIndex()
+        owlCarouselWrapper.style.transition = `all 0.3s ease`;
+        owlCarouselWrapper.style.transform = `translateX(-${(owlSingleCards[index].clientWidth * index) + (owlSingleCards[index].clientWidth * 2)}px)`;
+    }, 3000);
 
-        if (owlSingleCards[index].id === 'lastImg') {
-            owlCarouselWrapper.style.transition = `none`;
-            index = 0
-            owlCarouselWrapper.style.transform = `translateX(-${(owlSingleCards[index].clientWidth * index) + (owlSingleCards[index].clientWidth * 2)}px`;
+    function checkIndex() {
+        owlCarouselWrapper.addEventListener('transitionend', () => {
 
-        }
-    });
-}
+            if (owlSingleCards[index].id === 'lastImg') {
+                owlCarouselWrapper.style.transition = `none`;
+                index = 0
+                owlCarouselWrapper.style.transform = `translateX(-${(owlSingleCards[index].clientWidth * index) + (owlSingleCards[index].clientWidth * 2)}px`;
+
+            }
+        });
+    }
+})
--- script.js
+++ script.js
@@ -1,46 +1,57 @@
 
 const hello = document.getElementById('hello');
 const getnav = document.getElementById('sec-nav');
+
+
+
+//mobile nav
+const secnav = document.getElementById('sec-ul')
+function hee() {
+	if (getnav.classList.contains('active')) {
+		getnav.classList.remove('active');
+
+	} else {
+		getnav.classList.add('active');
+
+	}
+}
+
+
+let getblog = document.getElementById('drop');
+let getpage = document.getElementById('down');
+function blog() {
+	if (getblog.classList.contains('active')) {
+		getblog.classList.remove('active');
+	} else {
+		getblog.classList.add('active');
+	}
+
+}
+function page() {
+	if (getpage.classList.contains('active')) {
+		getpage.classList.remove('active');
+	} else {
+		getpage.classList.add('active');
+	}
+
+}
+
+const mainframe = document.querySelector('.mainframe');
+const header = document.querySelector('.header');
 const biggie = document.getElementById('biggie');
 
-		const secnav = document.getElementById('sec-ul')
-		function hee() {
-			if (getnav.classList.contains('active')) {
-				getnav.classList.remove('active');
 
-			} else {
-				getnav.classList.add('active');
+biggie.style.transition = 'all 0.5s linear';
+window.addEventListener('scroll', (e) => {
 
-			}
-		}
+	//distance from document top
+	const distanceFromTop = e.srcElement.scrollingElement.scrollTop;
 
-		let getblog = document.getElementById('drop');
-		let getpage = document.getElementById('down');
-		function blog() {
-			if (getblog.classList.contains('active')) {
-				getblog.classList.remove('active');
-			} else {
-				getblog.classList.add('active');
-			}
+	if (distanceFromTop > 0 && distanceFromTop < (mainframe.offsetTop - 500)) {
+		biggie.style.top = '-50px'
+	}
+	else if (distanceFromTop > (mainframe.offsetTop - 500)) {
+		biggie.style.top = '0px'
+	}
 
-		}
-		function page() {
-			if (getpage.classList.contains('active')) {
-				getpage.classList.remove('active');
-			} else {
-				getpage.classList.add('active');
-			}
-
-		}
-        
-        // window.addEventListener('scroll',function(){
-        //     let val = hello.getBoundingClientRect().bottom;
-        //     if(val <= 0){
-		// 		biggie.style.position = 'sticky';
-        //         biggie.style.transform = 'translateY(0%)';
-        //     }else{
-		// 		biggie.style.position = 'absolute';
-        //         biggie.style.transform = 'translateY(-100%)';
-        //     }
-        // })
-        
+})
